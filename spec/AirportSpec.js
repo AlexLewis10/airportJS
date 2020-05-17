06c805ec0e09e6cacb25cbc974b1f35324d537eb
Alex Lewis <alexlewis374@gmail.com>
2020-05-17 12:50:22 +0100

    basic test - plane has landed

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -6,8 +6,14 @@ describe('Airport', function() {
     })
 
     describe('Airport hangar is an array', function() {
-        it ('returns an array', function() {
+        it('returns an array', function() {
             expect(heathrow.hangar).toEqual([])
+        })
+    })
+
+    describe('Plane can land at the airport', function() {
+        it('plane lands', function() {
+            expect(heathrow.landPlane()).toEqual('Plane has landed')
         })
     })
 
--- src/Airport.js
+++ src/Airport.js
@@ -2,8 +2,11 @@ function Airport() {
     this.hangar = []
 }
 
-
+Airport.prototype.landPlane = function() {
+    return 'Plane has landed'
+}
 
 Airport.prototype.takeOff = function() {
     return 'plane has taken off'
 }
+
